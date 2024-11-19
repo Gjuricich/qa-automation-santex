893501c727dd6b74b9b0e7b1e8f3cf25aac8b1a1
Guada Juricich <guadajuricich@gmail.com>
2024-11-19 12:48:52 -0300

    First test - Login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (user, password) => { 
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    cy.get('input[name="username"]').type(user);
+    cy.get('input[name="password"]').type(password);
+    cy.get('.oxd-button').click();
+ })
